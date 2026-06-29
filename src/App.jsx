0ef590977307d08f5bb3c4096c2364413ef897e2
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-06-30 00:36:58 +0330

    initialize added to project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-[var(--text-secondary)]">
+        Hello, Vite!
+      </h1>
+    </>
+  );
+}
+
+export default App;
